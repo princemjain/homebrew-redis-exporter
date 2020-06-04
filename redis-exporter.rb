3726d0a97c18e4fe5674baa3309f7ad6e01dab35
Prince M Jain <prince.jain.aux@go-jek.com>
2020-06-04 15:59:28 +0800

    updating reference to url

--- redis-exporter.rb
+++ redis-exporter.rb
@@ -1,7 +1,7 @@
 class RedisExporter < Formula
   desc "Export data from Redis Cluster to CSV"
   homepage ""
-  url "https://github.com/princemjain/redis-exporter/archive/v1.0.0.tar.gz"
+  url "https://github.com/princemjain/redis-exporter/blob/master/redis-exporter-1.0.0.tar.gz"
   sha256 "d5c1d87a9f4f7bcb5c4799a99a66efad7d8ebb0346f8178e66b618d6fb173a1e"
   version "1.0.0"
   def install
