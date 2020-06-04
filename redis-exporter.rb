197c961f2ee00beed5431bb37295cd8692cb9715
Prince M Jain <prince.jain.aux@go-jek.com>
2020-06-04 18:44:02 +0800

    Prince | updating version to 2.0.0

--- redis-exporter.rb
+++ redis-exporter.rb
@@ -1,15 +1,16 @@
 class RedisExporter < Formula
   desc ""
   homepage ""
-  url "https://github.com/princemjain/redis-exporter/raw/master/redis-exporter-1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "fca30c333ab3adddf466446bd900fbb71ebaf84508a56517c206b26953dafb04"
+  url "https://github.com/princemjain/redis-exporter/raw/master/redis-exporter-2.0.0.tar.gz"
+  version "2.0.0"
+  sha256 "751805a7271e06c8d83ad55f2979a5e96ee2bcd2d30282a3825771b190eccbb6"
+  # depends_on "cmake" => :build
 
   def install
     bin.install "redis-exporter"
   end
 
   test do
-    assert_equal %x{redis-exporter -v}, "Version: 1.0.0\n"
+    assert_equal %x{redis-exporter -v}, "Version: 2.0.0\n"
   end
 end
