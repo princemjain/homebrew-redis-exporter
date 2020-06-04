4da518cdf40a2918a05dcf6ed2141568fd6001b9
Prince M Jain <prince.jain.aux@go-jek.com>
2020-06-04 17:45:35 +0800

    Prince | Fixing installation error

--- redis-exporter.rb
+++ redis-exporter.rb
@@ -1,19 +1,19 @@
 class RedisExporter < Formula
-  desc "Export data from Redis Cluster to CSV"
+  desc ""
   homepage ""
   url "https://github.com/princemjain/redis-exporter/raw/master/redis-exporter-1.0.0.tar.gz"
-  sha256 "5d575646b2bd4374712d4a0ac0cb44b54ed2fac2257ec52fae1f146c0abdbd9e"
   version "1.0.0"
+  sha256 "5d575646b2bd4374712d4a0ac0cb44b54ed2fac2257ec52fae1f146c0abdbd9e"
+
   def install
     system "./configure", "--disable-debug",
-           "--disable-dependency-tracking",
-           "--disable-silent-rules",
-           "--prefix=#{prefix}"
-    
-    bin.install "redis-exporter"
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
   end
 
   test do
-    assert_equal %x{redis-exporter -v}, "Version: 1.0.0\n"
+    system "false"
   end
 end
