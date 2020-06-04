b82b6fb6244b5d4991416f258a5f449b8cc6f04f
Prince M Jain <prince.jain.aux@go-jek.com>
2020-06-04 17:27:55 +0800

    Prince | Fixing installation error

--- redis-exporter.rb
+++ redis-exporter.rb
@@ -5,6 +5,11 @@ class RedisExporter < Formula
   sha256 "5d575646b2bd4374712d4a0ac0cb44b54ed2fac2257ec52fae1f146c0abdbd9e"
   version "1.0.0"
   def install
+    system "./configure", "--disable-debug",
+           "--disable-dependency-tracking",
+           "--disable-silent-rules",
+           "--prefix=#{prefix}"
+    
     bin.install "redis-exporter"
   end
 
