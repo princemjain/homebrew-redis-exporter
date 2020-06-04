83746eb828c5fba34dff2496fb2b3688f27304eb
Prince M Jain <prince.jain.aux@go-jek.com>
2020-06-04 16:13:01 +0800

    Prince | updating checksum

--- redis-exporter.rb
+++ redis-exporter.rb
@@ -2,7 +2,7 @@ class RedisExporter < Formula
   desc "Export data from Redis Cluster to CSV"
   homepage ""
   url "https://github.com/princemjain/redis-exporter/blob/master/redis-exporter-1.0.0.tar.gz"
-  sha256 "6d70115783915107652cdb0fae360549995dc68c7af7a4092a2b18d917f92281"
+  sha256 "4ce8ee5813ece9595d97f061391d0043cc88510a67c708e273ed059aada17e07"
   version "1.0.0"
   def install
     bin.install "redis-exporter"
