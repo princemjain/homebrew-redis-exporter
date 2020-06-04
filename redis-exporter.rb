68df8d4e5b8a60444d848926f32f44474fbfeb7f
Prince M Jain <prince.jain.aux@go-jek.com>
2020-06-04 18:35:04 +0800

    Prince | updating checksum

--- redis-exporter.rb
+++ redis-exporter.rb
@@ -3,7 +3,7 @@ class RedisExporter < Formula
   homepage ""
   url "https://github.com/princemjain/redis-exporter/raw/master/redis-exporter-1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "5d575646b2bd4374712d4a0ac0cb44b54ed2fac2257ec52fae1f146c0abdbd9e"
+  sha256 "fca30c333ab3adddf466446bd900fbb71ebaf84508a56517c206b26953dafb04"
 
   def install
     bin.install "redis-exporter"
